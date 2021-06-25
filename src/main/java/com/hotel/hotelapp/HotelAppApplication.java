6c6ffec1ee31a4f0024badf7f62fcbb7f38af242
Franco <francoagustinbarilatti@hotmail.com>
2021-06-24 21:31:25 -0300

    add class User with her Controller and Service

--- src/main/java/com/hotel/hotelapp/HotelAppApplication.java
+++ src/main/java/com/hotel/hotelapp/HotelAppApplication.java
@@ -3,11 +3,14 @@ package com.hotel.hotelapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HotelAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HotelAppApplication.class, args);
     }
+
+
 
 }
---
+++ src/main/java/com/hotel/hotelapp/user/UserService.java
@@ -0,0 +1,21 @@
+package com.hotel.hotelapp.user;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class UserService {
+    public List<User> getUser(){
+        return List.of( new User(
+                1L,
+                "Agustina",
+                "agusbasso.17@gmail.com",
+                LocalDate.of(1996, Month.AUGUST, 30),
+                24));
+    }
+
+}
